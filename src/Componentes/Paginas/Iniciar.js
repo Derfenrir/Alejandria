f29ab1da7fcb5c88bf15902eac7de1e4baff5c21
Gary <fran.2010montes@gmail.com>
2022-07-05 17:33:40 -0500

    Comprobacion de actuaizacion por repeticion de datos, con esta actualizacion se comprueba que se esta subiendo de manera correcta

--- src/Componentes/Paginas/Iniciar.js
+++ src/Componentes/Paginas/Iniciar.js
@@ -3,7 +3,23 @@ import React, { Component } from 'react'
 export class Iniciar extends Component {
   render() {
     return (
-      <div><h1>Hola Gary</h1></div>
+      <div className='container'>
+        <h1>
+  <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label"> Correo Electronico. </label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" class="form-text"> Nunca compartiremos su correo electrónico con nadie más. </div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label"> Contraseña. </label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <div id="emailHelp" class="form-text"> Nunca compartiremos su contraseña con nadie más.</div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+        </h1>
+      </div>
     )
   }
 }
--- src/Componentes/Paginas/Registrar.js
+++ src/Componentes/Paginas/Registrar.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react'
 export class Registrar extends Component {
   render() {
     return (
-      <div><h1>Ya quedo</h1></div>
+      <div>
+       <div className='container'>
+          <h1>
+          <form>
+  <div class="mb-3">
+    <label  class="form-label"> Usuario. </label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+
+  <div class="mb-3">
+    <label  class="form-label"> Correo Electronico. </label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <div id="emailHelp" class="form-text"> Nunca compartiremos su correo electrónico con nadie más.</div>
+  </div>
+
+  <div class="mb-3">
+    <label class="form-label"> Contraseña. </label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <div id="emailHelp" class="form-text"> Nunca compartiremos su contraseña con nadie más.</div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+          </h1>
+        </div>
+      </div>
     )
   }
 }
