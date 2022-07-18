7083ab0ccc40b51b200a3e9f7710e93059612a7c
alejandro <alejandrosr543@gmail.com>
2022-07-18 16:01:07 -0500

    cambio de servidor

--- src/Services/ConexionAxios.js
+++ src/Services/ConexionAxios.js
@@ -1,4 +1,4 @@
 import Axios from "axios";
 export default Axios.create({
-   baseURL: 'http://localhost:4000/api/'
+   baseURL: 'https://server-ale-heroku-22.herokuapp.com/api/'//'http://localhost:4000/api/'
 })
