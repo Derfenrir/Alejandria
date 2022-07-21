7298e41790fec78f985870dbde916848c6b66ffd
alejandro <alejandrosr543@gmail.com>
2022-07-21 12:37:36 -0500

    a

--- src/Services/ConexionAxios.js
+++ src/Services/ConexionAxios.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
 export default Axios.create({
-   baseURL:  'http://localhost:4000/api'//'https://server-ale-heroku-22.herokuapp.com/api/'
+   baseURL:  'https://server-ale-heroku-22.herokuapp.com/api/'//'http://localhost:4000/api'
 })
 
